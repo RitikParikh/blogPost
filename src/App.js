a68bf7557e36aea103efb72e65be03f1b631fba2
RitikParikh <ritik.parikh@gig4ce.com>
2022-10-10 17:37:48 +0530

    Ritik:First Push

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Basic from "./components";
+
+
+function App() {
+  return (
+    <Basic/>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Basic() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Basic
